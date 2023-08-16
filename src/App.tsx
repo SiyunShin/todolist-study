12ce68d865c14963d54e4fb8d79659ecd7d3e2e2
신시윤/콘텐츠프로덕트실 <siyun.shin@musinsa.com>
2023-08-16 20:52:16 +0900

    feat(app): 할 일 등록 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
+export type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
 function App() {
-  return <>개발환경세팅</>;
+  const [inputText, setInputText] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmitTodo = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (inputText) {
+      setTodos([
+        ...todos,
+        { id: Math.random() * 1000, text: inputText, completed: false },
+      ]);
+      setInputText("");
+      return;
+    }
+    alert("할 일을 입력해주세요");
+  };
+
+  return (
+    <div>
+      <h1>할 일 목록</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="할 일을 입력 해주세요."
+          onChange={(e) => handleInputText(e)}
+          value={inputText}
+        />
+        <button type="submit" onClick={(e) => handleSubmitTodo(e)}>
+          등록
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
